cfaddd0ec751bbd875b1086553e245a1d6497c75
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-25 02:19:53 +0900

    Add subscription - 21/02/25

--- server/routes/video.js
+++ server/routes/video.js
@@ -5,6 +5,7 @@ const { auth } = require("../middleware/auth");
 const multer = require("multer");
 const path = require("path");
 var ffmpeg = require("fluent-ffmpeg");
+const { Subscriber } = require("../models/Subscriber");
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -108,6 +109,32 @@ router.post("/getVideoDetail", (req, res) => {
       if (err) return res.status(400).send(err);
       res.status(200).json({ success: true, videoDetail });
     });
+});
+
+router.post("/getSubscriptionVideos", (req, res) => {
+
+  // 자신의 아이디를 가지고 구독하는 사람들을 찾는다.
+  Subscriber.find({ userFrom: req.body.userFrom })
+  .exec(( err, subscriberInfo ) => {
+    if(err) return res.status(400).send(err);
+
+    let subscribedUser = [];
+
+    subscriberInfo.map((subscriber, i) => {
+      subscribedUser.push(subscriber.userTo);
+    })
+
+    // 찾은 사람들의 비디오를 가지고 온다.
+    Video.find({ writer : {$in: subscribedUser} })
+    .populate('writer')
+    .exec((err, videos) => {
+      if(err) return res.status(400).send(err)
+      res.status(200).json({ success: true, videos })
+    })
+
+  })
+
+
 });
 
 module.exports = router;
